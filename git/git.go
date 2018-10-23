964e0c6a087d9ecae81f88a6c53f2aaf2fa4e300
jamlee <1575103047@qq.com>
2018-10-23 21:04:15 +0800

    fixed k8s.io package parse error
    when use package from k8s.io,  gotit got  `parse meta tag error`

--- git/git.go
+++ git/git.go
@@ -18,7 +18,7 @@ import (
 )
 
 var urlRegex = regexp.MustCompile(`([A-Za-z0-9_.-]+((/[A-Za-z0-9_.-]+)+?)?)/?(/info/refs|/git-upload-pack|\?go-get=1)`)
-var repoRegex = regexp.MustCompile(`content="(.+?) git (.+)?"`)
+var repoRegex = regexp.MustCompile(`content="(.+?)\s*git\s*(.+)?"`)
 
 // NewServer create a Server instance
 // The gopath should be a valid folder and will store git repositories later
